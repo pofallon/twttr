c331b44ced24858a065e2b302915a3c4789923af
Paul O'Fallon <pofallon@users.noreply.github.com>
2020-07-04 16:37:25 -0400

    Correctly authorize payloads

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import crypto from 'crypto'
 import OAuth from 'oauth-1.0a'
 import axios, { AxiosResponse } from 'axios'
 
+axios.defaults.baseURL = 'https://api.twitter.com/'
+
 export interface TwitterCredentials {
   consumerKey: string;
   consumerSecret: string;
@@ -11,11 +13,8 @@ export interface TwitterCredentials {
 
 export class TwitterClient {
 
-  public baseUrl = 'https://api.twitter.com/'
-  public creds: TwitterCredentials
-
   constructor (creds: TwitterCredentials) {
-    this.creds = creds
+
     const oauth = new OAuth({
       consumer: {
         key: creds.consumerKey,
@@ -24,20 +23,25 @@ export class TwitterClient {
       signature_method: 'HMAC-SHA1',
       hash_function (baseString, key): string {
         return crypto.createHmac('sha1', key).update(baseString).digest('base64')
+      },
+      body_hash_function (data): string {
+        return crypto.createHash('sha1').update(data).digest('base64')
       }
     })
-    axios.interceptors.request.use((config) => {
+
+    axios.interceptors.request.use(config => {
       config.headers = oauth.toHeader(oauth.authorize({
         url: `${config.baseURL}${config.url}`,
         method: config.method || 'GET',
-        data: config.data
+        data: config.data,
+        includeBodyHash: true
       }, {
-        key: this.creds.accessToken || '',
-        secret: this.creds.accessTokenSecret || ''
+        key: creds.accessToken || '',
+        secret: creds.accessTokenSecret || ''
       }))
       return config
     })
-    axios.defaults.baseURL = this.baseUrl
+
   }
 
   get(api: string): Promise<AxiosResponse> { 
